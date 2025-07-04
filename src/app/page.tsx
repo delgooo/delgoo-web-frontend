849d01f3817860c8f641f1782b06203d410d8f83
gcorrado208 <giuseppecorrado20082003@gmail.com>
2025-07-04 21:07:12 +0200

    testestest (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Ciao ancora NICOLO'. Karim vuoi approvare?
+            Ciao ancora NICOLO'. Karim vuoi approvare? No
           </li>
         </ol>
 
