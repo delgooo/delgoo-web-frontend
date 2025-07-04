0f2ad1e2cfdd4be19b6549cebcfa27d9e06c4ee4
Karim-M03 <116445594+Karim-M03@users.noreply.github.com>
2025-07-04 20:15:09 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Ciao ancora giuseppe.
+            Ciao ancora
           </li>
         </ol>
 
